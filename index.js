5618e59f634c63e52a8062b052d8b5f65f3113bc
benBen <benmbenjamin@gmail.com>
2016-09-28 16:11:59 -0700

    push item

--- index.js
+++ index.js
@@ -15,13 +15,28 @@ app.get('/party', function(req, res){
   res.sendFile(__dirname + '/party-form.html');
 });
 
-var io = require('socket.io')(server);
-io.on('connection', function(socket) {
+var io = require('socket.io')(server),
+    io_party = io.of('/');
+    io_party_form = io.of('/party');
+    els = [];
+
+io_party_form.on('connection', function(socket) {
   console.log('hello girl');
+
   socket.on('img', function(data){
     console.log(data);
+    var el = { src: data.src };
+    els.push(el);
+    io_party.emit('push', el)
   });
   socket.on('text', function(data){
     console.log(data);
+    var el = { src: data.src };
+    els.push(el);
+    io_party.emit('push', el)
   });
-})
+});
+
+io_party.on('connection', function(socket) {
+  console.log('party!');
+});
