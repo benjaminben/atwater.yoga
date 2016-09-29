e95e24f099a9e51234e1571b1d67960bcb3293a7
Ben Benjamin <benmbenjamin@gmail.com>
2016-09-28 20:51:37 -0400

    flub

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express'),
     app     = express();
 
 
-app.set('port', process.env.PORT || 8000);
+app.set('port', process.env.PORT || 8080);
 var server = app.listen(app.get('port'), function(){
   console.log('party on', app.get('port'));
 });
