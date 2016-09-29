8e479981716581b7324ddacc565b30ed96f9bef6
benBen <benmbenjamin@gmail.com>
2016-09-29 12:30:25 -0700

    bug squishes

--- index.js
+++ index.js
@@ -20,28 +20,42 @@ var io = require('socket.io')(server),
     io_party_form = io.of('/party');
     els = [];
 
+app.get('/els', function(req, res){
+  res.send(els);
+});
+
 io_party_form.on('connection', function(socket) {
   console.log('hello girl');
 
-  socket.on('img', function(data){
-    console.log(data);
-    var el = { type: 'img', src: data.src };
-    io_party.emit('push', el)
-  });
-  socket.on('text', function(data){
-    console.log(data);
-    var el = { type: 'text', src: data.src };
-    io_party.emit('push', el)
+  // socket.on('img', function(data){
+  //   console.log(data);
+  //   var el = { type: 'img', src: data.src };
+  //   io_party.emit('push', el)
+  // });
+  // socket.on('text', function(data){
+  //   console.log(data);
+  //   var el = { type: 'text', src: data.src };
+  //   io_party.emit('push', el)
+  // });
+
+  socket.on('el', function(data){
+    els.push(data.html);
+    io_party.emit('push', data);
   });
 });
 
 io_party.on('connection', function(socket) {
   console.log('party!');
 
-  io_party.emit('els', els);
+  // socket.on('open', function(id, data){
+  //   console.log('id party',id);
+  //   socket.broadcast.to(id).emit('els', els);
+  // });
 
-  socket.on('el', function(data) {
-    els.push(data.html);
-    console.log('els', els);
-  });
+  // socket.broadcast.to(id).emit('els', els);
+
+  // socket.on('el', function(data) {
+  //   els.push(data.html);
+  //   console.log('els', els);
+  // });
 });
